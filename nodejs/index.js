8e2f4c9ab72cc433a1092c04e7b38ba18e068540
MaralTach <yoloten.maral@gmail.com>
2024-06-27 15:26:18 +0300

    first commit

---
+++ nodejs/index.js
@@ -0,0 +1,11 @@
+// a = 5;
+// b = 430;
+// console.log(b+a)
+// console.log(a)
+
+// ((function f(n){return ((n > 1) ? n * f(n-1) : n)})(5));
+console.log(0.1 + 0.2); 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); 
+console.log(1 < 2 < 3); true
+console.log(3 > 2 > 1);false
+
