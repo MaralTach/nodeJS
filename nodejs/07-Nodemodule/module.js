5f1505deedf41dac22f6eecafe0e4b47af48b270
MaralTach <yoloten.maral@gmail.com>
2024-06-30 00:39:01 +0300

    1

---
+++ nodejs/07-Nodemodule/module.js
@@ -0,0 +1,36 @@
+"use strict"
+/* -------------------------------------------- */
+// MODULES
+/* -------------------------------------------- */
+// Dosyayı içe aktarma:
+
+// require('./modules/index.js')
+// require('./modules/index')
+// require('./modules/') // default file name: index
+
+/* -------------------------------------------- */
+// Import from Export
+
+// const test = require('./modules/')
+// test()
+
+// require('./modules/')()
+
+/* -------------------------------------------- */
+
+// const arrFunc = require('./modules/')
+// // console.log(arrFunc)
+// arrFunc[0]()
+// arrFunc[1]()
+// arrFunc[2]()
+
+// Array Destructuring:
+const [test1, test2, test3] = require('./modules/')
+test1()
+test2()
+test3()
+
+/* -------------------------------------------- */
+/* -------------------------------------------- */
+/* -------------------------------------------- */
+/* -------------------------------------------- */
