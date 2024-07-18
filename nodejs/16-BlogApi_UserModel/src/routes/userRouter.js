3d733f54e3023581e041695643260cb33bfaf3ea
MaralTach <yoloten.maral@gmail.com>
2024-07-18 20:08:50 +0300

    up

---
+++ nodejs/16-BlogApi_UserModel/src/routes/userRouter.js
@@ -0,0 +1,13 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const router = require('express').Router()
+
+
+router.route('/')
+.get()
+.post()
+
+module.exports = router
