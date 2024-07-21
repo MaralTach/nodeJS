a962d2c66653ad242cb8d69e1366c5be9d7e94a9
MaralTach <yoloten.maral@gmail.com>
2024-07-21 18:31:07 +0300

    crpt

--- nodejs/16-BlogApi_UserModel/src/models/userModel.js
+++ nodejs/16-BlogApi_UserModel/src/models/userModel.js
@@ -5,6 +5,25 @@
 
 const mongoose = require('mongoose')
 
+
+// Password Encrypt (PBKDF2 Method):
+// https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+
+const crypto = require('node:crypto')   //2 nokta ustuste install yapmaya gerek yok 
+
+// Parameters:
+const keyCode = process.env.SECRET_KEY // Şifreleme anahtarı.
+const loopCount = 10_000 // Döngü sayısı
+const charCount = 32 // write 32 for 64
+const encType = 'sha512' // Şifreleme algoritması.
+
+// Return encrypted password:
+const passwordEncrypt = function (password) {
+
+    return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+
+}
+
 /* ------------------------------------------------------- */
 
 const UserSchema = new mongoose.Schema({
@@ -20,6 +39,9 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
+        set: (password)=>{
+            return 'maral'
+        }    //veri kaydederken return edilen data kaydi
     },
 
     firstName: String,
