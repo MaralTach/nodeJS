12fd0fd21377c827647bba53e2415bccfadbe7c9
MaralTach <yoloten.maral@gmail.com>
2024-07-29 14:04:27 +0300

    up

--- nodejs/17-BlogApi_SessionCookies/src/middlewares/userControl.js
+++ nodejs/17-BlogApi_SessionCookies/src/middlewares/userControl.js
@@ -14,6 +14,7 @@ const { User } = require('../models/userModel')
 
 
 //! Middlewareler index.js de cagirildiginda  mumkin oldukca route'lerden once cagirilmalidir
+
 module.exports = async (req,res,next) =>{
 
     // console.log(req.session)
