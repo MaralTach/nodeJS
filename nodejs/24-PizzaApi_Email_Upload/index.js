87ed3ae04c7bdd4041f4552383435393574ed70c
MaralTach <yoloten.maral@gmail.com>
2024-08-02 16:52:39 +0300

    up

--- nodejs/24-PizzaApi_Email_Upload/index.js
+++ nodejs/24-PizzaApi_Email_Upload/index.js
@@ -139,7 +139,7 @@ app.all("/", (req, res) => {
 app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
