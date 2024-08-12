071c36741215fb00d80e37cb81e17fbb92408a9b
MaralTach <yoloten.maral@gmail.com>
2024-08-12 20:59:23 +0300

    tokens

--- nodejs/27-StockApi/src/routes/index.js
+++ nodejs/27-StockApi/src/routes/index.js
@@ -11,9 +11,9 @@ const router = require('express').Router()
 // // auth:
 // router.use('/auth', require('./auth'))
 // // user:
-// router.use('/users', require('./user'))
+router.use('/users', require('./user'))
 // // token:
-// router.use('/tokens', require('./token'))
+router.use('/tokens', require('./token'))
 
 
 // // brand:
