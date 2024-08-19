7a24044a9711425c0ac5eafcef622e427022e0d0
MaralTach <yoloten.maral@gmail.com>
2024-08-19 23:11:17 +0300

    dockers

--- nodejs/29-Docker-1/hello.js
+++ nodejs/29-Docker-1/hello.js
@@ -1,1 +1,7 @@
+// VMSs Dockers
+
+//!  1. dockers terminalde calistirmak   ->  node hello.js(filename.js)
+//!  2.  dockerfile. => docker build . => docker image
+//!  3.  dockerhub devreye giryor FROM node:current-alpine3.20  nodejs ortaminda alpine 3.2 kullandik 
+
 console.log('hello dockers');
