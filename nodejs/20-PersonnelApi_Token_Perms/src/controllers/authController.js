28a327a4bab634e90d1d47b4a1577905a8bfc799
MaralTach <yoloten.maral@gmail.com>
2024-08-27 01:29:21 +0300

    token

--- nodejs/20-PersonnelApi_Token_Perms/src/controllers/authController.js
+++ nodejs/20-PersonnelApi_Token_Perms/src/controllers/authController.js
@@ -4,6 +4,7 @@
 ------------------------------------------------------- */
 
 const Porsonnel = require('../models/personnel.model')
+const Token = require('../models/token.model')
 
 const passwordEncrypt = require('../helpers/passwordEncrypt')
 /*----------------------------------------------------- */
@@ -12,7 +13,34 @@ const passwordEncrypt = require('../helpers/passwordEncrypt')
 module.exports ={
     login:async (req,res) =>{
 
+        const {username, password} = req.body
 
+        if (username & password){
+
+            const user = await Personnel.findOne({ username })
+
+            if (user && user.password == passwordEncrypt(password)){
+
+                if (user.isActive) {
+                    
+                    
+
+                } else {
+                    
+                    res.errorStatusCode = 401
+                    throw new Error('User is not active')
+                }
+
+            }else {
+                res.errorStatusCode = 401
+            throw new Error('Wrong username or password')
+            }
+
+        }else{
+
+            res.errorStatusCode = 401
+            throw new Error('Please enter username and password')
+        }
     },
     
     logout:async (req,res) =>{
